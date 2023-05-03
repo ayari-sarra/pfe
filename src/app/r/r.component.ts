c2eb3d24a37658bdadc2a1a983f309cc9800e764
ayari-sarra <119888084+ayari-sarra@users.noreply.github.com>
2023-05-03 23:16:52 +0100

    first commit

---
+++ src/app/r/r.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-r',
+  templateUrl: './r.component.html',
+  styleUrls: ['./r.component.css']
+})
+export class RComponent {
+
+}
